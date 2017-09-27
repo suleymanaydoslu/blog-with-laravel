bf0fb1e96e4d688babef4ece88fcd8f1900ab1d4
Süleyman Aydoslu <suleymanaydoslu@hotmail.com>
2017-09-27 15:22:28 +0300

    admin, web and auth controllers are written

---
+++ app/Http/Controllers/BasePanelController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Route;
+
+class BasePanelController extends Controller
+{
+    /**
+     * @var string
+     */
+    protected $view_path;
+    /**
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * @param null $view
+     * @param array $data
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    protected function view($view = null, $data = [])
+    {
+        $data = array_merge($this->data, $data);
+        $view = 'panel.' . $this->view_path . '.' . $view;
+        return view($view, $data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('panel/login',[
+    'as' => 'panel.login',
+    'uses' => 'AuthController@loginForPanel'
+]);
+
+/**
+ * Including panel routes
+ */
+Route::group([
+    'middleware' => ['panel'],
+    'prefix' => '',
+], function () {
+    /**
+     * Application Routes
+     */
+    require __DIR__ . '/panel.php';
 });
